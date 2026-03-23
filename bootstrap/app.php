48f14b3a0a72d724b06e903b21e01e8589705fa9
Marco020413 <marf1382003@gmail.com>
2026-03-23 20:17:57 +0000

    Register Firebase Service Provider

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -5,6 +5,10 @@ use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
 return Application::configure(basePath: dirname(__DIR__))
+    ->withProviders([
+        \App\Providers\FirebaseServiceProvider::class,
+    ])
+    
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
         commands: __DIR__.'/../routes/console.php',
@@ -15,4 +19,5 @@ return Application::configure(basePath: dirname(__DIR__))
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
+        
     })->create();
